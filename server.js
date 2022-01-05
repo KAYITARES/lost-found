20400be0a70c717f349e25eb94ab9dd29f7831c3
KAYITARE-C <ani7kayitare@gmail.com>
2022-01-05 11:45:57 +0200

    my first push

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+
+dotenv.config('./.env');
+
+const app=express();
+
+app.use("/",(req,res)=>
+res.status(200).json({
+    message:"This is a wrong LostFound APi",
+})
+);
+const dbUrl=process.env.DATABASEURL;
+mongoose.connect(dbUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+
+}).then(()=>console.log("Database successfully connected"))
+const port=process.env.PORT;
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+});
+export default app;

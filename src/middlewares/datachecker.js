8b2af4ef9d9e2356c8065d442e5df33bb7b7edca
KAYITARE-C <ani7kayitare@gmail.com>
2022-01-13 15:53:52 +0200

    middleware for foundlost

--- src/middlewares/datachecker.js
+++ src/middlewares/datachecker.js
@@ -1,31 +1,72 @@
 import UserInfos from "../models/user";
-import DocumentTypeInfos from "../models/documentType"
+import DocumentTypeInfos from "../models/documentType";
+import documentInfos from "../models/document";
+import lostFoundInfos from "../models/lostFound";
 
-class Datachecker{
+class Datachecker {
+  //check if user PHONE exist
 
-    //check if user PHONE exist
+  static async isMobilePhoneNumberExist(req, res, next) {
+    const user = await UserInfos.findOne({ phone: req.body.phone });
 
-static async isMobilePhoneNumberExist (req,res,next){
-
-    const user = await UserInfos.findOne({phone:req.body.phone});
-
-  
-
-if(!user) {
-    return next();
-}
-return  res.status(401).json ({error:"phone already exist!"})
-
-}
-static async isDocumentTypeNameExist(req,res,next){
-    const documentTypeName=await DocumentTypeInfos.findOne({name:req.body.name});
-    if(!documentTypeName){
-        return next();
+    if (!user) {
+      return next();
     }
-    return res.status(401).json({error:"document type name already exist"})
-}
+    return res.status(401).json({ error: "phone already exist!" });
+  }
+  static async isDocumentTypeNameExist(req, res, next) {
+    const documentTypeName = await DocumentTypeInfos.findOne({
+      name: req.body.name,
+    });
+    if (!documentTypeName) {
+      return next();
+    }
+    return res.status(401).json({ error: "document type name already exist" });
+  }
+
+  static async isDocumentExist(req, res, next) {
+    const userDocument = await documentInfos.findOne({
+      IDofthedocument: req.body.IDofthedocument,
+    });
+
+    console.log(userDocument)
+
+    if (!userDocument) {
+      return next();
+    } else if (
+      (userDocument.status == "Lost" && req.body.status == "Found") ||
+      (userDocument.status == "Found" && req.body.status == "Lost")
+    ) {
+      await documentInfos.findByIdAndUpdate(userDocument._id, {
+        status: "Approved",
+      });
+
+      //send sms to user Lost userFound
+
+      let docData = {
+        documentId: userDocument._id,
+        userId: req.user._id,
+      };
+
+      const lostFoundData = await lostFoundInfos.create(docData);
+
+      const data = await lostFoundInfos.findById(lostFoundData._id);
+
+      return res.status(200).json({
+          message:"document lost is found succesfully",
+          data:data
+      })
+    }
 
 
+    else{
+        return res.status(404).json({
+            error:"failed to register document"
+        })
+    }
+
+
+  }
 }
 
 export default Datachecker;
--- src/routes/documentRoutes.js
+++ src/routes/documentRoutes.js
@@ -9,26 +9,32 @@ import verifyToken from "../middlewares/verifyToken";
 import User from "../models/user";
 import userRoutes from "../routes/userRoutes";
 
-const docRouter =express.Router();
+const docRouter = express.Router();
 
-docRouter.post("/register",
-verifyToken,
-verifyAccess("userLost"),
-DocController.createDoc)
-docRouter.get("/all",
-verifyToken,
-verifyAccess("admin"),
-DocController.getAllDocument)
-docRouter.get("/:id",
-verifyToken,
-verifyAccess("admin"),
-DocController.getOneDoc)
-docRouter.delete("/:id",
-verifyToken,
-verifyAccess("admin"),
-DocController.deletOneDoc)
-docRouter.patch("/:id",DocController.changeDocument)
-
-
+docRouter.post(
+  "/register",
+  verifyToken,
+  DataChecker.isDocumentExist,
+  DocController.createDoc
+);
+docRouter.get(
+  "/all",
+//   verifyToken,
+//   verifyAccess("admin"),
+  DocController.getAllDocument
+);
+docRouter.get(
+  "/:id",
+  verifyToken,
+  verifyAccess("admin"),
+  DocController.getOneDoc
+);
+docRouter.delete(
+  "/:id",
+  verifyToken,
+  verifyAccess("admin"),
+  DocController.deletOneDoc
+);
+docRouter.patch("/:id", DocController.changeDocument);
 
 export default docRouter;
